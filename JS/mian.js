07d4ef13d15c7c6124e98a5f4ebb714a5bba9c4d
Ralabdulwa98 <rmalabdulwahab@gmail.com>
2020-04-12 04:29:48 +0300

    fix Contact Page

--- JS/mian.js
+++ JS/mian.js
@@ -1,9 +1,21 @@
 
-
 //witch between pages 
 $('#switchHome').click(function () {
+    $('nav').toggleClass('menu menu_active');
     window.location.href = 'Home.html';
 });
-$('#switchAbout').click(function () {
+$('#switchEducationandQualification').click(function () {
     window.location.href = 'EducationandQualification.html';
+});
+$('#switchCourse').click(function () {
+    window.location.href = 'Course.html';
+});
+$('#switchExperience').click(function () {
+    window.location.href = 'Experience.html';
+});
+$('#switchProjects').click(function () {
+    window.location.href = 'Projects.html';
+});
+$('#switchContact').click(function () {
+    window.location.href = 'Contact.html';
 });
